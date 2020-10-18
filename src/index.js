f0fd7d2964873db9160f50f253b704e14d2def9e
Danil Besputin <besputindanil@yandex.ru>
2020-10-18 11:59:43 +0400

    feat: add solution to brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openConfigs = [];
+  let closeConfigs = [];
+
+  bracketsConfig.forEach(item => {
+    openConfigs.push(item[0]);
+    closeConfigs.push(item[1]);
+  });
+
+  let result = [];
+  
+  for (let i = 0; i < str.length; i++) {
+    let j;
+
+    if (closeConfigs.includes(str[i]) && result.length > 0) {
+      j = closeConfigs.indexOf(str[i]);
+      if (result[result.length - 1] == openConfigs[j]) {
+        result.pop();
+      } else {
+        result.push(str[i]);
+      }
+    } else if (openConfigs.includes(str[i])) {
+      result.push(str[i]);
+    } else {
+      return false
+    }
+  }
+  return result.length === 0
 }
